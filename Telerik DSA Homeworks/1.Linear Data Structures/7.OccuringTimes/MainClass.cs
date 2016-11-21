132ad0c19b8b93404b3e80e5dd381b16166ef654
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-11-22 00:17:15 +0200

    Exercise 7 ready

---
+++ Telerik DSA Homeworks/1.Linear Data Structures/7.OccuringTimes/MainClass.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * Write a program that finds in given array of integers (all belonging to the range [0..1000]) 
+ * how many times each of them occurs.
+ * 
+ * Example: array = {3, 4, 4, 2, 3, 3, 4, 3, 2}
+ *      2 → 2 times
+ *      3 → 4 times
+ *      4 → 3 times
+ * 
+ *  https://github.com/TelerikAcademy/Data-Structures-and-Algorithms/tree/master/Topics/02.%20Linear-Data-Structures/homework
+ */
+namespace _7.OccuringTimes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class OccuringTimes
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
