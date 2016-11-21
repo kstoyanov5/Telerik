b9b89da4cb1e51b715a3f64b97097eaebfa6c6b5
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-11-22 00:14:16 +0200

    Exercis 6 finished

---
+++ Telerik DSA Homeworks/1.Linear Data Structures/6.RemoveOdds/Main.cs
@@ -0,0 +1,30 @@
+﻿/**
+ *  Write a program that removes from given sequence all numbers that occur odd number of times.
+ *  
+ *  Example:
+ *  {4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2} → {5, 3, 3, 5}
+ *  
+ *  https://github.com/TelerikAcademy/Data-Structures-and-Algorithms/tree/master/Topics/02.%20Linear-Data-Structures/homework
+ */
+
+namespace _6.RemoveOdds
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class RemoveOdds
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>(new int[] { 4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2 });
+
+            var evenNumbers = list.GroupBy(x => x).Where(x => x.Count() % 2 == 0).Select(g => g.Key);
+
+            foreach (var item in evenNumbers)
+            {
+                Console.WriteLine(item + " ");
+            }
+        }
+    }
+}
