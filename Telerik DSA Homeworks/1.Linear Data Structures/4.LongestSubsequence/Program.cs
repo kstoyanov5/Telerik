291c6538b1d6a3ee44629569d0b8d9698e9a499f
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-11-21 16:50:29 +0200

    Exercise 4 setup

---
+++ Telerik DSA Homeworks/1.Linear Data Structures/4.LongestSubsequence/Program.cs
@@ -0,0 +1,19 @@
+﻿/**
+ *  Write a method that finds the longest subsequence of 
+ *  equal numbers in given List and returns the result as new List<int>.
+ *  
+ *  https://github.com/TelerikAcademy/Data-Structures-and-Algorithms/tree/master/Topics/02.%20Linear-Data-Structures/homework
+ */
+
+namespace _4.LongestSubsequence
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
