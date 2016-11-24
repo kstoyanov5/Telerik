67fd79cceff903bea54152e74fd35510b3b58554
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-11-24 18:28:41 +0200

    Exercise 5 ready

--- Telerik DSA Homeworks/2.Recursion/5.OrderedSubsets/StartUp.cs
+++ Telerik DSA Homeworks/2.Recursion/5.OrderedSubsets/StartUp.cs
@@ -10,13 +10,45 @@
 namespace _5.OrderedSubsets
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
 
     class StartUp
     {
+        static string[] set = { "hi", "a", "b" };
+        static int[] subSets;
+
+        static int N = set.Length;
+        static int K = 2;
+
         static void Main(string[] args)
         {
+            subSets = new int[N];
+
+            recursiveLoop(0);
+        }
+
+        static void recursiveLoop(int index)
+        {
+            if (index >= K)
+            {
+                Print();
+                return;
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                subSets[index] = i;
+                recursiveLoop(index + 1);
+            }
+
+        }
+
+        static void Print()
+        {
+            for (int i = 0; i < K; i++)
+            {
+                Console.Write(set[subSets[i]] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
