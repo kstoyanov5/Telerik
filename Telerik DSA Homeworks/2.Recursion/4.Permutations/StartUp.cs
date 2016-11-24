490c32cf49204eb5acbe7d2e8970cd01beb19b37
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-11-24 17:54:19 +0200

    Exercise 4 refactor and Exercise 5 setup

--- Telerik DSA Homeworks/2.Recursion/4.Permutations/StartUp.cs
+++ Telerik DSA Homeworks/2.Recursion/4.Permutations/StartUp.cs
@@ -1,4 +1,12 @@
-﻿
+﻿/*
+ * Write a recursive program for generating and printing all permutations of the
+ * numbers 1, 2, ..., n for given integer number n. 
+ * 
+ * Example: n=3 → {1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2},{3, 2, 1}
+ * 
+ * https://github.com/TelerikAcademy/Data-Structures-and-Algorithms/tree/master/Topics/03.%20Recursion/homework
+ */
+
 namespace _4.Permutations
 {
     using System;
---
+++ Telerik DSA Homeworks/2.Recursion/5.OrderedSubsets/StartUp.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Write a recursive program for generating and printing all ordered k-element subsets
+ * from n-element set (variations Vkn).
+ * 
+ * Example: n=3, k=2, set = {hi, a, b} → (hi hi), (hi a), (hi b), (a hi), (a a), (a b), (b hi), (b a), (b b)
+ * 
+ * https://github.com/TelerikAcademy/Data-Structures-and-Algorithms/tree/master/Topics/03.%20Recursion/homework
+ */
+
+namespace _5.OrderedSubsets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
