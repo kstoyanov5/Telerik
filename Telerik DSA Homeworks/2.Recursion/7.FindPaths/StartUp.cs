b6df887d039125dc752ade351a94fc5aaec0cb49
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-11-24 19:24:21 +0200

    Exercise 7 setup

---
+++ Telerik DSA Homeworks/2.Recursion/7.FindPaths/StartUp.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * We are given a matrix of passable and non-passable cells.
+ * Write a recursive program for finding all paths between two cells in the matrix.
+ * https://github.com/TelerikAcademy/Data-Structures-and-Algorithms/tree/master/Topics/03.%20Recursion/homework
+ */
+namespace _7.FindPaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
