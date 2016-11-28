e12a8a6170463258e6fc7831bc8d6ac7c4052267
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-11-28 23:48:06 +0200

    Problem 1 solved

--- Telerik DSA Homeworks/3.Combinatorics/1.DoublePasswords/Program.cs
+++ Telerik DSA Homeworks/3.Combinatorics/1.DoublePasswords/Program.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _1.DoublePasswords
+﻿namespace _1.DoublePasswords
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
-        {
+        {                      
+            string templatePassword = Console.ReadLine();
+
+            int unknownDigits = 0;
+
+            for (int i = 0; i < templatePassword.Length; i++)
+            {
+                if (templatePassword[i] == '*')
+                {
+                    unknownDigits++;
+                }
+            }
+
+            long answer = 1;
+            for (int i = 1; i <= unknownDigits; i++)
+            {
+                answer *= 2;
+            }
+            Console.WriteLine(answer);
         }
     }
 }
