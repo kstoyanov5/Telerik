f024bee96623ae12d1bc3d8cb56c208686fff971
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-11-29 13:17:25 +0200

    Problem 2 solved

--- Telerik DSA Homeworks/Workshop1/2.ReversePolishNotation/Program.cs
+++ Telerik DSA Homeworks/Workshop1/2.ReversePolishNotation/Program.cs
@@ -1,15 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _2.ReversePolishNotation
+﻿namespace _2.ReversePolishNotation
 {
+    using System;
+    using System.Collections.Generic;
+
     class Program
     {
         static void Main(string[] args)
         {
+            string[] input = Console.ReadLine().Split(' ');
+            Stack<long> inputProcessor = new Stack<long>();
+
+            foreach (var item in input)
+            {
+                switch (item)
+                {
+                    case "+":
+                        inputProcessor.Push(inputProcessor.Pop() + inputProcessor.Pop());
+                        break;
+                    case "-":
+                        {
+                            long temp = inputProcessor.Pop();
+                            inputProcessor.Push(inputProcessor.Pop() - temp);
+                            break;
+                        }
+                    case "*":
+                        inputProcessor.Push(inputProcessor.Pop() * inputProcessor.Pop());
+                        break;
+                    case "/":
+                        {
+                            long temp = inputProcessor.Pop();
+                            inputProcessor.Push(inputProcessor.Pop() / temp);
+                            break;
+                        }
+                    case "&":
+                        inputProcessor.Push(inputProcessor.Pop() & inputProcessor.Pop());
+                        break;
+                    case "|":
+                        inputProcessor.Push(inputProcessor.Pop() | inputProcessor.Pop());
+                        break;
+                    case "^":
+                        inputProcessor.Push(inputProcessor.Pop() ^ inputProcessor.Pop());
+                        break;
+                    default:
+                        inputProcessor.Push(long.Parse(item));
+                        break;
+                }
+            }
+            Console.WriteLine(inputProcessor.Pop());
         }
     }
 }
