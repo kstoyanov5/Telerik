0f10de9dd312fd80076f05a149c9b0f31c762ef4
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-12-01 11:24:29 +0200

    Exercise 1.Multiverse setup

---
+++ Exam prep C-Sharp-2/1.MultiverseCommunication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.MultiverseCommunication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var digits = new[] { "CHU", "TEL", "OFT", "IVA", "EMY", "VNB", "POQ", "ERI", "CAD", "K-A", "IIA", "YLO", "PLA" };
+        }
+    }
+}
