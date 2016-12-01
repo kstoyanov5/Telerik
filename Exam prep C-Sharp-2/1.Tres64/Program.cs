39b2c118037738d31e4157dcb3f77b1420c08d84
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-12-01 09:47:30 +0200

    C Sharp Advanced Exam prep. Solved problem 1 from 2014 exam

---
+++ Exam prep C-Sharp-2/1.Tres64/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1.Tres64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong input = ulong.Parse(Console.ReadLine());
+
+            var numeralSystem = 9;
+
+            var digits = new[] { "LON+", "VK-", "*ACAD", "^MIM", "ERIK|", "SEY&", "EMY>>", "/TEL", "<<DON" };
+
+            StringBuilder result = new StringBuilder();
+            if (input == 0)
+            {
+                Console.WriteLine(digits[0]);
+            }
+            else
+            {
+                while (input > 0)
+                {
+                    int digitIn9th = (int)(input % (ulong)numeralSystem);
+                    result.Insert(0, digits[digitIn9th]);
+                    input /= (ulong)numeralSystem;
+                }
+                Console.WriteLine(result.ToString());
+            }
+        }
+    }
+}
