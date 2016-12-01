9bb64e56c4aa79f548b1322875a6713365b31d3b
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-12-01 12:08:00 +0200

    Another style of solving MultiverseCommunication

---
+++ Exam prep C-Sharp-2/1.MultiverseCommunication-SecondStyle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Numerics;
+
+namespace _1.MultiverseCommunication_SecondStyle
+{
+    class Program
+    {
+        static BigInteger Pow(int number, int power)
+        {
+            int result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().Replace("CHU", "0")
+                                             .Replace("TEL", "1")
+                                             .Replace("OFT", "2")
+                                             .Replace("IVA", "3")
+                                             .Replace("EMY", "4")
+                                             .Replace("VNB", "5")
+                                             .Replace("POQ", "6")
+                                             .Replace("ERI", "7")
+                                             .Replace("CAD", "8")
+                                             .Replace("K-A", "9")
+                                             .Replace("IIA", ":")
+                                             .Replace("YLO", ";")
+                                             .Replace("PLA", "<");
+            int power = 0;
+            BigInteger result = 0;
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                int currentNumber = input[i] - '0';
+                result += currentNumber * Pow(13, power);
+                power++;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
