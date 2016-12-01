dde8f6e794828ce77f30383dc96dababa65f726e
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-12-01 11:16:17 +0200

    Exam exercise StrangelandNumbers solved

--- Exam prep C-Sharp-2/1.StrangelandNumbers/Program.cs
+++ Exam prep C-Sharp-2/1.StrangelandNumbers/Program.cs
@@ -1,22 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace _1.StrangelandNumbers
 {
     class Program
     {
+        static BigInteger Pow(int number, int power)
+        {
+            int result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            uint input = uint.Parse(Console.ReadLine());
+            string input = Console.ReadLine().Replace("f", "0")
+                                             .Replace("bIN", "1")
+                                             .Replace("oBJEC", "2")
+                                             .Replace("mNTRAVL", "3")
+                                             .Replace("lPVKNQ", "4")
+                                             .Replace("pNWE", "5")
+                                             .Replace("hT", "6");
+            BigInteger inputResult = 0;
+            int power = 0;
 
-            var digits = new[] { "f", "bIN", "oBJEC", "mNTRAVL", "lPVKNQ", "pNWE", "hT" };
-
-            var numeralSystem = 7;
-
-            StringBuilder result = new StringBuilder();
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                int currentNumber = input[i] - '0';
+                inputResult += currentNumber * Pow(7, power);
+                power++;
+            }
+            Console.WriteLine(inputResult);
         }
     }
 }
