5b6834e93a4d7ff9a974f9189f879438b41250f8
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-12-02 12:46:35 +0200

    Exam task CalculationProblem solved

--- Exam prep C-Sharp-2/1.CalculationProblem/CalculationProblem.cs
+++ Exam prep C-Sharp-2/1.CalculationProblem/CalculationProblem.cs
@@ -8,22 +8,48 @@ namespace _1.CalculationProblem
     class CalculationProblem
     {
         static List<string> digits = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w" };
+        static int numeralSystem = 23;
 
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
 
+            string[] splits = input.Split(' ');
+            StringBuilder builder = new StringBuilder();
+            List<BigInteger> final = new List<BigInteger>();
+
             BigInteger result = 0;
-            int numeralSystem = 19;
 
-            for (int i = 0; i < input.Length; i++)
+            for (int i = 0; i < splits.Length; i++)
             {
-                var currentNum = input.Substring(i, 1);
-                var decimalValue = digits.IndexOf(currentNum);
+                for (int j = 0; j < splits[i].Length; j++)
+                {
+                    var currentNum = splits[i].Substring(j, 1);
+                    var decimalValue = digits.IndexOf(currentNum);
 
-                result *= numeralSystem;
-                result += decimalValue;
+                    result *= numeralSystem;
+                    result += decimalValue;
+                }
+                final.Add(result);
+                result = 0;
             }
+
+            BigInteger total = 0;
+            foreach (var item in final)
+            {
+                total += item;
+            }
+            Console.WriteLine("{0} = {1}", ConvertFromDecimal(total, 23), total);
+        }
+
+        static string ConvertToInt(string word)
+        {
+            string result = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                result += digits.IndexOf(word[i].ToString());
+            }
+            return result;
         }
 
         static BigInteger Pow(int number, int power)
@@ -36,5 +62,59 @@ namespace _1.CalculationProblem
             return result;
         }
 
+        static BigInteger ConvertToDecimal(string number, int baseFrom)
+        {
+            BigInteger decimalNumber = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] > '9')
+                {
+                    decimalNumber += (number[i] - '7') * Pow(baseFrom, (number.Length - 1 - i));
+                }
+                else
+                {
+                    decimalNumber += (number[i] - '0') * Pow(baseFrom, (number.Length - 1 - i));
+                }
+            }
+            return decimalNumber;
+        }
+
+        static String ConvertFromDecimal(BigInteger number, int baseTo)
+        {
+            List<BigInteger> result = new List<BigInteger>();
+            StringBuilder builder = new StringBuilder();
+            if (baseTo > 10)
+            {
+                while (number > 0)
+                {
+                    result.Add(number % baseTo);
+                    number = number / baseTo;
+                }
+                result.Reverse();
+                foreach (int item in result)
+                {
+                    //Console.Write(digits[item]);
+                    builder.Append(digits[item]);
+                }
+                //Console.Write(" ");
+                return builder.ToString();
+            }
+            else
+            {
+                while (number > 0)
+                {
+                    result.Add(number % baseTo);
+                    number = number / baseTo;
+                }
+                result.Reverse();
+                foreach (int item in result)
+                {
+                    //Console.Write(digits[item]);
+                    builder.Append(digits[item]);
+                }
+                return builder.ToString();
+                //Console.Write(" ");
+            }
+        }
     }
 }
---
+++ Exam prep C-Sharp-2/ExamProblem1/Problem1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamProblem1
+{
+    class Problem1
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
