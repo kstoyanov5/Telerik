a3e2f568d2fc305f9cc3d64e49374a04396068bb
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-12-02 16:28:44 +0200

    Solved problem 2 IncreasingAbsoluteDifferences

---
+++ Exam prep C-Sharp-2/2.EvenDifferences/EvenDifferences.cs
@@ -0,0 +1,40 @@
+﻿namespace _2.EvenDifferences
+{
+    using System;
+    using System.Linq;
+
+    class EvenDifferences
+    {
+        // 1. read input
+        // 2. put the input numbers in a collection
+        // 3. iterate over the collection and calculate the sum
+        // 3.1 find absolute difference of current and the previous number
+        // 3.2 if even difference, sum it
+        // 3.3 make a jump - even or odd
+        // 4. print the sum
+
+        static void Main(string[] args)
+        {
+            var sequence = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+
+            long sum = 0;
+            int index = 1;
+
+            while (index < sequence.Length)
+            {
+                long absDiff = Math.Abs(sequence[index] - sequence[index - 1]);
+
+                if (absDiff % 2 == 0)
+                {
+                    sum += absDiff;
+                    index += 2;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Exam prep C-Sharp-2/2.IncreasingAbsoluteDifferences/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _2.IncreasingAbsoluteDifferences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = int.Parse(Console.ReadLine());
+            List<string> output = new List<string>();
+
+            while (line > 0)
+            {
+                var input = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+                bool isIncreasing = true;
+
+                for (int j = 2; j < input.Length; j++)
+                {
+                    var absDiff = Math.Abs(input[j - 1] - input[j]);
+                    var lastAbsDiff = Math.Abs(input[j - 2] - input[j - 1]);
+
+                    if (lastAbsDiff > absDiff || absDiff - lastAbsDiff > 1) 
+                    {
+                        isIncreasing = false;
+                        break;
+                    }
+                }
+                output.Add(isIncreasing.ToString());
+                line--;
+            }
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
