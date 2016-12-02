0e5ae891624945d5b5564d3b2f82a7b9aec076bf
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-12-02 22:19:30 +0200

    Problem 2.MagicWords setup and 2.Kitty semi-solution

---
+++ Exam prep C-Sharp-2/2.Kitty/Kitty.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Kitty
+{
+    class Kitty
+    {
+        static void Main(string[] args)
+        {
+
+            int souls = 0;
+            int food = 0;
+            int deadlocks = 0;
+
+            string input = Console.ReadLine();
+            string path = Console.ReadLine();
+
+
+            for (int position = 0; position < input.Length; position++)
+            {
+                if (input[position] == '@')
+                {
+                    souls++;
+                }
+                else if (input[position] == '*')
+                {
+                    food++;
+                }
+                else if(input[position] == 'x')
+                {
+                    if (input[position] % 2 == 0)
+                    {
+                        souls--;
+                    }
+                    else
+                        food--;
+                }
+            }
+
+            Console.WriteLine("Souls" + souls);
+            Console.WriteLine("Food" + food);
+            Console.WriteLine("Deadlocks" + deadlocks);
+        }
+    }
+}
