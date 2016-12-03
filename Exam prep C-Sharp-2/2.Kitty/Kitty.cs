1d8daabb23daca308febd7be11e2189d873ccdaa
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-12-03 14:45:13 +0200

    Problem 2.Kitty solved

--- Exam prep C-Sharp-2/2.Kitty/Kitty.cs
+++ Exam prep C-Sharp-2/2.Kitty/Kitty.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _2.Kitty
 {
@@ -10,39 +7,80 @@ namespace _2.Kitty
     {
         static void Main(string[] args)
         {
+            long souls = 0;
+            long food = 0;
+            long deadlocks = 0;
 
-            int souls = 0;
-            int food = 0;
-            int deadlocks = 0;
+            char[] input = Console.ReadLine().ToCharArray();
+            var path = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
 
-            string input = Console.ReadLine();
-            string path = Console.ReadLine();
+            bool[] boolArray = new bool[input.Length];
+            long jumps = 0;
+            int position = 0;
+            long temp = 0;
 
-
-            for (int position = 0; position < input.Length; position++)
+            for (int i = 0; i <= path.Length; i++)
             {
-                if (input[position] == '@')
+                if (input[position] == '@' && boolArray[position] == false)
                 {
                     souls++;
+                    boolArray[position] = true;
                 }
-                else if (input[position] == '*')
+                else if (input[position] == '*' && boolArray[position] == false)
                 {
                     food++;
+                    boolArray[position] = true;
                 }
-                else if(input[position] == 'x')
+                else if (input[position] == 'x')
                 {
-                    if (input[position] % 2 == 0)
+                    deadlocks++;
+                    if (position % 2 == 0)
                     {
+                        if (souls <= 0)
+                        {
+                            Console.WriteLine("You are deadlocked, you greedy kitty!\nJumps before deadlock: {0}", jumps);
+                            break;
+                        }
                         souls--;
+                        input[position] = '@';
                     }
                     else
+                    {
+                        if (food <= 0)
+                        {
+                            Console.WriteLine("You are deadlocked, you greedy kitty!\nJumps before deadlock: {0}", jumps);
+                            break;
+                        }
                         food--;
+                        input[position] = '*';
+                    }
                 }
-            }
+                if (i == path.Length)
+                {
+                    Console.WriteLine("Coder souls collected: {0}", souls);
+                    Console.WriteLine("Food collected: {0}", food);
+                    Console.WriteLine("Deadlocks: {0}", deadlocks);
+                    break;
+                }
 
-            Console.WriteLine("Souls" + souls);
-            Console.WriteLine("Food" + food);
-            Console.WriteLine("Deadlocks" + deadlocks);
+                temp += path[i];
+                if (temp < 0)
+                {
+                    while (temp < 0)
+                    {
+                        temp = input.Length + temp;
+                    }
+                }
+                else if (temp >= input.Length)
+                {
+                    while (temp >= input.Length)
+                    {
+                        temp %= input.Length;
+                    }
+                }
+                position = (int)temp;
+                jumps++;
+            }
         }
     }
 }
