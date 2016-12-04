bda9b2186c13f6364769215c2bcb3b80a66c42de
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-12-04 20:50:35 +0200

    Exam Problem 3 and 4 setups, added

---
+++ Exam prep C-Sharp-2/ExamProblem3/Problem3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamProblem3
+{
+    class Problem3
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
