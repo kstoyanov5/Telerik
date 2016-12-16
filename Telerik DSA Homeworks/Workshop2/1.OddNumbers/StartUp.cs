b0f09cd36d4d3025707379004757c33b4ba89ff3
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2016-12-16 17:23:32 +0200

    Workshop 2 - Trees, Graphs, Hash sets. Solution for problem 1

---
+++ Telerik DSA Homeworks/Workshop2/1.OddNumbers/StartUp.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * 
+ * Full description of the problem can be found at:
+ * https://github.com/TelerikAcademy/Data-Structures-and-Algorithms/blob/master/Workshops/2016/09-12-2016/OddNumber/README.md
+ * 
+ */
+namespace _1.OddNumbers
+{
+    using System;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            uint n = uint.Parse(Console.ReadLine());
+
+            long number = 0;
+            for (int i = 0; i < n; i++)
+            {
+                long currentNumber = long.Parse(Console.ReadLine());
+                number ^= currentNumber;
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
