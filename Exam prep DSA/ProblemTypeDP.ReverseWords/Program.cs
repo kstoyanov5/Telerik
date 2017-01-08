ea1e00ea118d8245912b1263e804319e9e176baa
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2017-01-08 16:23:51 +0200

    ReverseWord

---
+++ Exam prep DSA/ProblemTypeDP.ReverseWords/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ProblemTypeDP.ReverseWords
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Program
+    {
+        static void Main()
+        {
+            var someString = "A test sentence.";
+
+            Console.WriteLine(StringReverse(someString));
+        } 
+
+
+    }
+}
