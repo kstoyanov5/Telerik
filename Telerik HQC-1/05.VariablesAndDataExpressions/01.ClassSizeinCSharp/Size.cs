f132848e9bdb9927e96e960a76cc749007f46331
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2017-03-22 12:11:11 +0200

    HQC1 homework added

---
+++ Telerik HQC-1/05.VariablesAndDataExpressions/01.ClassSizeinCSharp/Size.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+/// <summary>
+/// This class to calculates the dimensions of a bounding
+/// box surrounding a rectangle which is rotated by given angle (alpha).
+/// </summary>
+public class Size
+{
+    private double width;
+    private double height;
+
+    public Size(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double Width
+    {
+        get
+        {
+            return this.width;
+        }
+
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Can't assign negative width.");
+            }
+
+            this.width = value;
+        }
+    }
+
+    public double Height
+    {
+        get
+        {
+            return this.height;
+        }
+
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Can't assign negative height.");
+            }
+
+            this.height = value;
+        }
+    }
+
+    /// <summary>
+    /// Returns the size (width, height) of the bounding box.
+    /// </summary>
+    /// <param name="size">The size of the rectangle.</param>
+    /// <param name="alpha">The angle of rotation.</param>
+    /// <returns>The dimensions of the bounding box.</returns>
+    public static Size GetRotatedSize(Size size, double alpha)
+    {
+        var sinAlpha = Math.Sin(alpha);
+        var cosAlpha = Math.Cos(alpha);
+
+        var absSinAlpha = Math.Abs(sinAlpha);
+        var absCosAlpha = Math.Abs(cosAlpha);
+
+        var originalWidth = size.Width;
+        var originalHeight = size.Height;
+
+        var rotatedWidth = (absSinAlpha * originalWidth) + (absCosAlpha * originalHeight);
+        var rotatedHeight = (absCosAlpha * originalWidth) + (absSinAlpha * originalHeight);
+
+        var rotatedSize = new Size(rotatedWidth, rotatedHeight);
+        return rotatedSize;
+    }
+}
---
+++ Telerik HQC-1/05.VariablesAndDataExpressions/2.PrintStatisticsInCSharp/StatisticsPrinter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/// <summary>
+/// This class to prints statistical data from an array. It includes
+/// the min, max and average values of a double array.
+/// </summary>
+static class StatisticsPrinter
+{
+    public static void PrintStatistics(double[] array)
+    {
+        int count = array.Length;
+        double max = double.MinValue;
+
+        for (int i = 0; i < count; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
+        }
+
+        PrintValue(max);
+
+        double min = double.MaxValue;
+
+        for (int i = 0; i < count; i++)
+        {
+            if (array[i] < min)
+            {
+                min = array[i];
+            }
+        }
+
+        PrintValue(min);
+
+        double sum = 0;
+        for (int i = 0; i < count; i++)
+        {
+            sum += array[i];
+        }
+
+        double average = sum / count;
+        PrintValue(average);
+    }
+
+    /// <summary>
+    /// Prints the value of the parameter on the console.
+    /// </summary>
+    private static void PrintValue(double value)
+    {
+        Console.WriteLine(value.ToString());
+    }
+}
